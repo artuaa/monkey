008b1f7605e1e86fae2aae6c8a20f6057866170e
artua <theartua@gmail.com>
2022-09-16 09:00:35 +0300

    Prefix Expressions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -21,8 +21,43 @@ func Eval(node ast.Node) object.Object {
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Value)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	}
 	return nil
+}
+
+func evalPrefixExpression(op string, right object.Object) object.Object {
+	switch op {
+	case "!":
+		return evalBangOperator(right)
+	case "-":
+		return evalMinusOperator(right)
+	}
+	return NULL
+}
+
+func evalMinusOperator(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
+}
+
+func evalBangOperator(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+
+	}
 }
 
 func evalStatements(node *ast.Program) object.Object {
